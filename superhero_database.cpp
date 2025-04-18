6345d94c327333f7451d486c87d9abe2f3bc4f15
VasquezTrinidee <vasqueztrinidee@gmail.com>
2025-04-18 10:58:04 -0500

    Create superhero_database.cpp

---
+++ superhero_database.cpp
@@ -0,0 +1,7 @@
+echo "# cpp-obscure-heroes" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/VasquezTrinidee/cpp-obscure-heroes.git
+git push -u origin main
